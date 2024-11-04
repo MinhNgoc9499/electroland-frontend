7b5f3b1a42e1c8478b824c01e4887d6210622549
MinhNgoc <minhngoc9499@gmail.com>
2024-11-04 23:23:39 +0700

    add origin

---
+++ JS/login.js
@@ -0,0 +1,15 @@
+const registerButton = document.getElementById("register");
+const loginButton = document.getElementById("login");
+const container = document.getElementById("container");
+const loginContainer = document.getElementsByClassName("login-container")[0]; // Access the first element directly
+
+registerButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+    loginContainer.style.opacity = "0"; // Set opacity to 0 to make it invisible
+});
+
+loginButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+    loginContainer.style.opacity = "1"; // Reset opacity to 1 to make it visible again
+});
+
